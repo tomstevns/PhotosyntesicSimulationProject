4690e2418e62c8b18e8263fda1011283a75996c6
Tom Stevns <tomstevns@gmail.com>
2023-04-04 11:46:10 +0200

    This is my first successful contribution
    The project goes like this, in danish:
    
    På bagrund af nedenstående om en fotosyntese model - efter kolon,  ønsker jeg at lave et Python program som kan simulere og plotte den anden ordens differential ligning for fotosyntesen: Fotosyntese er den proces, hvor planter og andre autotrofe organismer bruger sollys til at omdanne vand og kuldioxid til glukose og oxygen. Denne proces kan beskrives med en anden ordens differentialligning, der modellerer ændringen i mængden af glukose i løbet af tiden.
    Lad G(t) være mængden af glukose på tidspunktet t. Så kan vi definere en anden ordens differentialligning, der beskriver ændringen i G(t) over tid som:
    G''(t) = k * I(t) - r * G'(t)
    hvor G'(t) er den første afledte af G(t) og angiver hastigheden af ændringen i mængden af glukose, G''(t) er den anden afledte af G(t) og angiver accelerationen af ændringen i mængden af glukose, k er en konstant, der angiver fotosyntese effektiviteten, I(t) er intensiteten af sollys, som varierer med tiden, og r er en konstant, der angiver forbruget af glukose.
    Den første del af ligningen, k * I(t), repræsenterer produktionen af glukose gennem fotosyntese, mens den anden del, r * G'(t), repræsenterer forbruget af glukose af planten til energi og vækst. Denne ligning beskriver således balancen mellem produktion og forbrug af glukose i planter og kan bruges til at forudsige mængden af glukose på ethvert tidspunkt.

---
+++ venv/refinedPhotosyntheticSimulation.py
@@ -0,0 +1,40 @@
+#For at simulere og plotte denne anden ordens differentialligning
+# for fotosyntesen i Python, kan du følge disse trin:
+
+#Importer de nødvendige biblioteker, herunder numpy og matplotlib:
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+#Definer konstanterne k og r, og definer en funktion I(t) for at repræsentere intensiteten af sollys,
+# som varierer med tiden.
+# For eksempel kan du bruge en sinusfunktion for at simulere dagslysets variationer:
+k = 0.5
+r = 0.1
+
+def I(t):
+    return 100 * (1 + np.sin(t/12))
+
+#Definer den anden ordens differentialligning ved hjælp af en funktion,
+# og definer startværdierne for G(t) og G'(t):
+def G_diff_eq(G, t):
+    return np.array([G[1], k * I(t) - r * G[1]])
+
+G0 = np.array([0, 0]) # startværdier for G(t) og G'(t)
+
+#Brug scipy.integrate.odeint funktionen til at løse denne anden ordens differentialligning numerisk
+# og beregne G(t) for en række tidsværdier:
+t = np.linspace(0, 48, 1000) # tidsværdier fra 0 til 48 timer
+G = odeint(G_diff_eq, G0, t)
+
+#Plot resultatet ved hjælp af matplotlib:
+plt.plot(t, G[:, 0], label='Glucose') # plot G(t)
+plt.xlabel('Time (hours)')
+plt.ylabel('Glucose (mg)')
+plt.legend()
+plt.show()
+
+#Dette vil generere et plot af mængden af glukose over tid,
+# som varierer med sollysintensiteten og forbrugsraten.
+# Du kan herefter justere konstanterne og intensitetsfunktionen til at simulere
+# forskellige scenarier og observere, hvordan mængden af glukose varierer over tid.
